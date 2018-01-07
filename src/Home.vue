c85ba85b3d6635e3472bfa5a46827b6132e7199b
zmcdaniel <zmcdaniel@gmail.com>
2018-01-07 15:56:45 -0800

    adding components

---
+++ src/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <app-server-status></app-server-status>
+</template>
+
+<script>
+    import ServerStatus from './ServerStatus.vue';
+
+    export default {
+        components: {
+            AppServerStatus,
+            'app-server-status': ServerStatus
+        }
+    }
+
+</script>
